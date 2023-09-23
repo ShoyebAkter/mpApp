ab9e52b4dba467b2dd24da1b14bf8ca07ece19f3
Shoyeb Akter <heroreal5385@gmail.com>
2023-09-23 17:21:34 +0600

    allies page updated

--- src/Pages/LandingPage/Allies/Allies.jsx
+++ src/Pages/LandingPage/Allies/Allies.jsx
@@ -4,38 +4,45 @@ export const Allies = () => {
     {
       id: 1,
       name: "John Doe",
-      company: "ABC Inc.",
+      company: "Holismus",
       text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor tincidunt urna, eget gravida libero dapibus id."
+        "We put our trust in EulerMail and they delivered, making sure our needs were met and deadlines were always hit."
     },
     {
       id: 2,
       name: "Jane Smith",
-      company: "XYZ Co.",
+      company: "Angularis Creative",
       text:
-        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae."
+        "EulerMail enthusiasm coupled with their keen interest in our brands success made it a satisfying and enjoyable experience."
     },
     {
       id: 3,
       name: "Alice Johnson",
-      company: "123 Corp.",
+      company: "Maximo Jewelery",
       text:
-        "In eget sapien vitae massa efficitur vehicula. Proin interdum, ante eget tincidunt tincidunt, nunc mauris tristique justo, a varius lorem justo at purus."
+        "Incredible end result! Our sales increased over 400% when we worked with EulerMail. Highly recommended!"
+    },
+    {
+      id: 4,
+      name: "Sam Johnson",
+      company: "CORREDORES DE REASEGUROS",
+      text:
+        "Incredible end result! Our sales increased over 400% when we worked with EulerMail. Highly recommended!"
     }
   ];
   return (
       <section className="bg-gray-100 py-16">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-semibold text-center mb-8">Our Clients Love Us</h2>
+          <h2 className="text-3xl font-semibold text-center mb-8">Our Allies</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             {testimonialsData.map((testimonial) => (
               <div
                 key={testimonial.id}
-                className="bg-white rounded-lg shadow-md p-6 hover:shadow-lg transform hover:-translate-y-2 transition duration-300"
+                className="bg-white text-center rounded-lg shadow-md p-6 hover:shadow-lg transform hover:-translate-y-2 transition duration-300"
               >
-                <p className="text-gray-600 mb-4">{testimonial.text}</p>
+                <p className="text-gray-600 mb-4 font-medium">{testimonial.text}</p>
                 <div className="text-sm text-gray-500">
-                  <p className="font-semibold">{testimonial.name}</p>
+                  <p className="font-bold text-black text-xl">{testimonial.name}</p>
                   <p>{testimonial.company}</p>
                 </div>
               </div>

d331e8bff6855b25ab81052563be6c1178b2663e
Shoyeb Akter <heroreal5385@gmail.com>
2023-09-25 10:55:17 +0600

    overview chart updated

--- src/Pages/BusinessOverview/MiddleChart.jsx
+++ src/Pages/BusinessOverview/MiddleChart.jsx
@@ -1,47 +1,123 @@
-import ReactApexChart from "react-apexcharts";
+import { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
 
 export const MiddleChart = () => {
-    const options= {
-          
-        series: [{
-            name: "Desktops",
-            data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-        }],
-        options: {
-          chart: {
-            height: 350,
-            type: 'line',
-            zoom: {
-              enabled: false
-            }
-          },
-          dataLabels: {
-            enabled: false
-          },
-          stroke: {
-            curve: 'straight'
-          },
-          title: {
-            text: 'Product Trends by Month',
-            align: 'left'
-          },
-          grid: {
-            row: {
-              colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-              opacity: 0.5
-            },
-          },
-          xaxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
-          }
-        },
-      
-      
-      };
+  const [totalSales, setTotalSales] = useState([]);
+  const salesValue = [];
+  const resultArray = [];
+  useEffect(() => {
+    fetch('http://localhost:5000/sales')
+      .then(res => res.json())
+      .then(result => setTotalSales(result))
+      .catch(error => console.error(error))
+  }, [])
+  // console.log(totalSales);
+
+  const getSalesData = () => {
+    totalSales.map((sale) => {
+
+      const month = new Date(sale.sale.$date.$numberLong * 1000).getMonth();
+
+      const sales = sale.items.map((item) => {
+        const price = item.price.$numberDecimal;
+        const quantity = item.quantity.$numberInt;
+        const totalPrice = parseInt(price) * parseInt(quantity);
+        return totalPrice;
+      })
+      let sum = 0;
+      for (let i = 0; i < sales.length; i++) {
+        sum = sum + sales[i];
+      }
+
+      const obj = {
+        month: month,
+        total: sum
+      }
+      salesValue.push(obj)
+    })
+
+  }
+  getSalesData()
+  console.log(salesValue);
+  const changeArrayValue = () => {
+    const monthNames = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    salesValue.forEach((obj) => {
+      // Check if an object with the same 'month' property exists in the result array
+      const existingObject = resultArray.find((item) => item.month === obj.month);
+
+      if (existingObject) {
+        // If it exists, add the 'value' property
+        existingObject.total += obj.total;
+      } else {
+
+        // If it doesn't exist, create a new entry in the result array
+        resultArray.push({ month: obj.month, total: obj.total });
+      }
+    });
+    resultArray.sort((a, b) => a.month - b.month);
+    // if (monthNumber >= 1 && monthNumber <= 12) {
+    //   return monthNames[monthNumber - 1];
+    // } else {
+    //   return 'Invalid Month';
+    // }
+    const updatedArray = resultArray.map((result) => {
+      if (result.month >= 1 && result.month <= 12) {
+        return { ...result, month: monthNames[result.month - 1] }
+      } else {
+        return result
+      }
+    })
+    // console.log(resultArray);
+    return updatedArray;
+  }
+  const revenue = changeArrayValue()
+  // console.log(revenue);
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Monthly Revenue',
+      },
+    },
+  };
+
+  const labels = revenue.map((array) => array.month);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: `Sales $`,
+        data: revenue.map((array) => array.total),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      }
+    ],
+  };
   return (
-    <div id="chart" className='shadow-xl rounded-xl my-5'>
-        <h1 className='text-black text-xl text-center font-medium text-cyan-500'> Business Overview</h1>
-            <ReactApexChart className="flex justify-center" options={options} series={options.series} type="line" height={150} width={800} />
-        </div>
+    <div id="chart" style={{"height":"300px"}} className='flex justify-center shadow-xl rounded-xl my-5'>
+      <Line height={300}
+      width={1100}
+        options={options}
+        data={data}
+      />
+    </div>
   )
 }
--- src/Pages/EulerMail/OverviewChart.jsx
+++ src/Pages/EulerMail/OverviewChart.jsx
@@ -24,31 +24,93 @@ ChartJS.register(
 
 
 function OverviewChart() {
-  const transactions=[];
-  // const labels=[]
-  const[sales,setSales]=useState([])
-  useEffect(()=>{
-    fetch('../../../Sales.json').
-    then((res)=>res.json())
-    .then((result)=>setSales(result.sales))
-  },[])
+  const [totalSales, setTotalSales] = useState([]);
+  const salesValue = [];
+  useEffect(() => {
+    fetch('http://localhost:5000/sales')
+      .then(res => res.json())
+      .then(result => setTotalSales(result))
+      .catch(error => console.error(error))
+  }, [])
+  // console.log(totalSales);
 
-  const getValue=()=>{
-    sales.map((item)=>{
-      item.transactions.map((transaction)=>{
-        
-        const year =new Date(transaction.date.$date.$numberLong*1000).getFullYear()
-        const object={
-          year:year,
-          amount:Math.ceil(transaction.total)
-        }
-        transactions.push(object);
+  const getSalesData = () => {
+    totalSales.map((sale) => {
+
+      const day = new Date(sale.sale.$date.$numberLong * 1000).getDate();
+      const month = new Date(sale.sale.$date.$numberLong * 1000).getMonth();
+      const year = new Date(sale.sale.$date.$numberLong * 1000).getFullYear();
+
+      const formattedDay = day < 10 ? `0${day}` : day;
+      const formattedMonth = month < 10 ? `0${month}` : month;
+      const date=`${formattedMonth}/${formattedDay}/${year}`;
+      const sales = sale.items.map((item) => {
+        const price = item.price.$numberDecimal;
+        const quantity = item.quantity.$numberInt;
+        const totalPrice = parseInt(price) * parseInt(quantity);
+        return totalPrice;
       })
-    })
-  }
-  getValue()
+      let sum = 0;
+      for (let i = 0; i < sales.length; i++) {
+        sum = sum + sales[i];
+      }
 
-  // console.log(transactions);
+      const obj = {
+        date: date,
+        total: sum
+      }
+      salesValue.push(obj)
+    })
+
+  }
+  getSalesData()
+  console.log(salesValue);
+  // const changeArrayValue = () => {
+  //   const monthNames = [
+  //     'January',
+  //     'February',
+  //     'March',
+  //     'April',
+  //     'May',
+  //     'June',
+  //     'July',
+  //     'August',
+  //     'September',
+  //     'October',
+  //     'November',
+  //     'December',
+  //   ];
+  //   salesValue.forEach((obj) => {
+  //     // Check if an object with the same 'month' property exists in the result array
+  //     const existingObject = resultArray.find((item) => item.month === obj.month);
+
+  //     if (existingObject) {
+  //       // If it exists, add the 'value' property
+  //       existingObject.total += obj.total;
+  //     } else {
+
+  //       // If it doesn't exist, create a new entry in the result array
+  //       resultArray.push({ month: obj.month, total: obj.total });
+  //     }
+  //   });
+  //   resultArray.sort((a, b) => a.month - b.month);
+  //   // if (monthNumber >= 1 && monthNumber <= 12) {
+  //   //   return monthNames[monthNumber - 1];
+  //   // } else {
+  //   //   return 'Invalid Month';
+  //   // }
+  //   const updatedArray = resultArray.map((result) => {
+  //     if (result.month >= 1 && result.month <= 12) {
+  //       return { ...result, month: monthNames[result.month - 1] }
+  //     } else {
+  //       return result
+  //     }
+  //   })
+  //   // console.log(resultArray);
+  //   return updatedArray;
+  // }
+  // const revenue = changeArrayValue()
+  // console.log(revenue);
   const options = {
     responsive: true,
     plugins: {
@@ -57,21 +119,19 @@ function OverviewChart() {
       },
       title: {
         display: true,
-        text: 'Chart.js Line Chart',
+        text: 'Monthly Revenue',
       },
     },
   };
 
-  // const labels = transactions.map((tran,index)=>{ if(index<=4) return tran.year});
-  const labels = ["jan","feb","march","april"];
+  const labels = salesValue.map((sale) => sale.date);
 
   const data = {
     labels,
     datasets: [
       {
-        label: 'Dataset 1',
-        data:transactions.map((tran,index)=>{ if(index<=4) return tran.amount}),
-        // data:[10,20,30,40],
+        label: `Sales $`,
+        data: salesValue.map((sale) => sale.total),
         borderColor: 'rgb(255, 99, 132)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
       }

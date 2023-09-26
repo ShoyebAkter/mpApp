b2e2fe0c3e49c1bdabdceb67f0accdcdef3cea06
Shoyeb Akter <heroreal5385@gmail.com>
2023-09-26 12:18:16 +0600

    header changed

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -4,18 +4,20 @@ import './Header.css'
 import { Link } from 'react-router-dom';
 
 function Header() {
-	
+
 	return (
 		<header className="header-fixed">
 
-			<nav className='items-center'>
-				<div style={{"width":"50px"}} ><img  src='logo2.png'/></div>
-				<a href='/'>Home</a>
-				<a href='#businessInsight'>EulerMail</a>
-				<a href='#integration'>How it Works</a>
-				<a href='#allies'>Our Allies</a>
-				<a href='#contactUs'>Contact</a>
-				<Link className='bg-emerald-800 text-white py-2 px-5 rounded-xl' to="/login">Login</Link>
+			<nav className='flex justify-around'>
+				<div style={{ "width": "80px" }} ><img src='logo2.png' /></div>
+				<div className="flex items-center ">
+					<a href='/'>Home</a>
+					<a href='#businessInsight'>EulerMail</a>
+					<a href='#integration'>How it Works</a>
+					<a href='#allies'>Our Allies</a>
+					<a href='#contactUs'>Contact</a>
+					<Link className='bg-emerald-800 text-white py-2 px-5 rounded-xl' to="/login">Login</Link>
+				</div>
 			</nav>
 
 		</header>

23c60abe364aa2a690256a289d61f4929e1b8dba
Shoyeb Akter <heroreal5385@gmail.com>
2023-09-27 15:55:30 +0600

    not working react corusel

--- src/Pages/LandingPage/BusinessInsight/BusinessInsight.jsx
+++ src/Pages/LandingPage/BusinessInsight/BusinessInsight.jsx
@@ -1,34 +1,14 @@
-import { useState } from 'react';
+
 import './Business.css'
 
-
 export const BusinessInsight = () => {
-    const cardWidth = 300; // Adjust the card width according to your design
-    const totalCards = 5; // Total number of cards
-    const cardsPerSet = 3; // Number of cards per set
-    const totalSets = Math.ceil(totalCards / cardsPerSet);
-
-    const [currentSet, setCurrentSet] = useState(1);
-
-    const handleNextClick = () => {
-        if (currentSet < totalSets) {
-            setCurrentSet(currentSet + 1);
-        }
-    };
-
-    const handlePreviousClick = () => {
-        if (currentSet > 1) {
-            setCurrentSet(currentSet - 1);
-        }
-    };
-
-    const slidePosition = -1 * (currentSet - 1) * cardsPerSet * cardWidth;
-
+    
     return (
         <section className="container">
             <h1 className='text-center text-3xl font-medium text-green-700'>Our Services</h1>
             <section className="card__container">
-                <div className="card__bx" style={{ "--clr": "#89ec5b", "transform": `translateX(${slidePosition}px)` }}>
+                
+                <div className="card__bx" style={{ "--clr": "#89ec5b"}}>
                     <div className="card__data">
                         <div className="card__icon">
                             <i className="fa-solid fa-paintbrush"></i>
@@ -64,33 +44,30 @@ export const BusinessInsight = () => {
                         </div>
                     </div>
                 </div>
-                <div className="card__bx" style={{ "--clr": "#5b98eb" }}>
+                <div className="card__bx" style={{ "--clr": "#c4cdda" }}>
                     <div className="card__data">
                         <div className="card__icon">
                             <i className="fa-brands fa-searchengin"></i>
                         </div>
                         <div className="card__content">
-                            <h3>Business Data Analysis</h3>
+                            <h3>Campaign Creation</h3>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                             <a href="#">Read More</a>
                         </div>
                     </div>
                 </div>
-                <div className="card__bx" style={{ "--clr": "#5b98eb" }}>
+                <div className="card__bx" style={{ "--clr": "#aaa7a2" }}>
                     <div className="card__data">
                         <div className="card__icon">
                             <i className="fa-brands fa-searchengin"></i>
                         </div>
                         <div className="card__content">
-                            <h3>Business Data Analysis</h3>
+                            <h3>Email Marketing</h3>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                             <a href="#">Read More</a>
                         </div>
                     </div>
                 </div>
-                <button onClick={handlePreviousClick} className="slider-button">Previous</button>
-                <button onClick={handleNextClick} className="slider-button">Next</button>
-
             </section>
 
         </section>

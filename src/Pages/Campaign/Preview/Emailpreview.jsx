b99f8f7a97f643694f4cf1c54e5dab7f49133154
Shoyeb Akter <heroreal5385@gmail.com>
2023-10-04 10:27:50 +0600

    whatsapp preview changed

--- src/Pages/Campaign/Preview/Emailpreview.jsx
+++ src/Pages/Campaign/Preview/Emailpreview.jsx
@@ -3,8 +3,10 @@ import { ReactMultiEmail} from 'react-multi-email';
 import { useForm } from 'react-hook-form';
 import 'react-multi-email/dist/style.css';
 import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
-  
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
 export const Emailpreview = ({imageBlob,editedImage,text}) => {
     const {  register,handleSubmit,reset} = useForm();
     const [emails, setEmails] = useState([]);
@@ -33,7 +35,7 @@ export const Emailpreview = ({imageBlob,editedImage,text}) => {
               if(result.success){
                 const img=result.data.url;
                 const emailInfo={
-                    senderEmail:data.email,
+                    promotionType:data.type,
                     emails:emails,
                     message:text,
                     subject:data.subject,
@@ -59,15 +61,10 @@ export const Emailpreview = ({imageBlob,editedImage,text}) => {
                 <form className="bg-white p-10 rounded-xl text-white"onSubmit={handleSubmit(sendEmail)}>
                     
                     <div className="relative mb-6">
-                        <div className="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
-                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
-                                <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
-                                <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
-                            </svg>
-                        </div>
+                        
                         <input
-                        {...register("email")}
-                        type="email" name="email" id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="From: name@gmail.com" />
+                        {...register("type")}
+                        type="text" name="type" id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Promotion type" />
                     </div>
                     <ReactMultiEmail 
                     
--- src/Pages/Campaign/Preview/WhatsappPreview.jsx
+++ src/Pages/Campaign/Preview/WhatsappPreview.jsx
@@ -14,44 +14,17 @@ export const WhatsappPreview = ({editedImage,text}) => {
 
     // Replace non-alphanumeric characters and spaces in the mobile number
     let number = data.phone.replace(/[^\w\s]/gi, "").replace(/ /g, "");
-    console.log(number);
-   
+    // console.log(number);
+    const url= `https://wa.me/${number}?text=${encodeURIComponent(text)}`
     // URL to open WhatsApp with the message and image attachment
     
     // Check if a file has been selected
     // const fileInput = document.getElementById('myFile');
     // console.log(fileInput.files.length);
-    if (data.image.length > 0) {
-      console.log(data.image[0]);
-      const image = data.image[0];
-      const formData = new FormData();
-      formData.append('image', image);
-      const imagebburl = `https://api.imgbb.com/1/upload?key=${imageStorageKey}`;
-
-      fetch(imagebburl,{
-        method:'POST',
-        body:formData
-      }).then(res=>res.json())
-      .then(result=>{
-        if(result.success){
-          const img=result.data.url;
-          console.log(img);
-          const url= `https://wa.me/${data.phone}?text=${encodeURIComponent(data.message)}&amp;source=&amp;data=&amp;image=${encodeURIComponent(img)}`
-          // url += `&abid=${encodeURIComponent(img)}`;
-          console.log(url);
-          window.open(url);
-        }
-      })
-      // Assuming you have uploaded the image to a publicly accessible server,
-      // provide the URL of the image in the "abid" parameter
-      // Replace with your image URL
-      
-      // Add the image attachment to the URL
-
-    }
+    
 // console.log(url);
     // Open WhatsApp in a new tab with the updated URL
-    // 
+    window.open(url);
   };
 
 
@@ -61,10 +34,10 @@ export const WhatsappPreview = ({editedImage,text}) => {
         <form className="bg-white p-10 rounded-xl text-white" onSubmit={handleSubmit(onSubmitForm)}>
           <div className="relative mb-6">
             <input
-            {...register("phone")}
-              name="phone"
+            {...register("type")}
+              name="type"
               // value={mobileNumber}
-              type="number" id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="From: +8801" />
+              type="text" id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Promotion Type" />
           </div>
           <div className="relative mb-6">
             <input
@@ -73,12 +46,6 @@ export const WhatsappPreview = ({editedImage,text}) => {
               // value={mobileNumber}
               type="number" id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="To: +8801" />
           </div>
-
-
-          <div className="relative mb-6">
-            <img src={editedImage} alt='edited image'/>
-          </div>
-
           <div>
             <textarea
               name="message"

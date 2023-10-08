51cc44fd91888f633f5f2bfdf9c1223b77b4379d
Shoyeb Akter <heroreal5385@gmail.com>
2023-10-08 11:18:19 +0600

    campaign result section changed

--- src/Pages/CampaignResult/ButtonGroup.jsx
+++ src/Pages/CampaignResult/ButtonGroup.jsx
@@ -1,12 +1,14 @@
 
-export const ButtonGroup = () => {
+export const ButtonGroup = ({emailCampaign,whatsAppCampaign}) => {
+    console.log(emailCampaign,whatsAppCampaign);
+    const total=emailCampaign+whatsAppCampaign;
     return (
         <div className="flex justify-around">
             <button className="bg-green-600 text-white font-bold py-7 px-10 rounded">
-                Total Campaign
+                Total Campaign {total}
             </button>
             <button className="bg-green-600 text-white font-bold py-7 px-10 rounded">
-                Total Email Sent
+                Total Email Sent {emailCampaign}
             </button>
             <button className="bg-green-600 text-white font-bold py-7 px-10 rounded">
                 Total Unique Opener
--- src/Pages/CampaignResult/CampaignResult.jsx
+++ src/Pages/CampaignResult/CampaignResult.jsx
@@ -5,9 +5,29 @@ import { CampaignDetails } from "./CampaignDetails/CampaignDetails"
 import { ClickRatio } from "./ClickRatio/ClickRatio"
 import { useNavigate } from "react-router-dom";
 import Loading from "../Authentication/Loading";
+import { useEffect, useState } from "react";
 
 export const CampaignResult = () => {
   const [user,loading] = useAuthState(auth);
+  const [emailCampaign,setEmailCampaign]=useState([]);
+  const [whatsAppCampaign,setWhatsAppCampaign]=useState([]);
+  useEffect(()=>{
+    getWhatsAppCampaignData()
+  },[])
+
+  const getEmailCampaignData=()=>{
+    fetch(`https://emapp-backend.vercel.app/emailcampaign/${user.uid}`)
+    .then(res=>res.json())
+    .then(result=>setEmailCampaign(result))
+  }
+  
+  const getWhatsAppCampaignData=()=>{
+    fetch(`https://emapp-backend.vercel.app/whatsappcampaign/${user.uid}`)
+    .then(res=>res.json())
+    .then(result=>setWhatsAppCampaign(result))
+
+    getEmailCampaignData();
+  }
     const navigate=useNavigate()
     if(loading) return <Loading></Loading>
     if(!user){
@@ -15,7 +35,7 @@ export const CampaignResult = () => {
     }
   return (
     <div className="text-black mt-10">
-        <ButtonGroup/>
+        <ButtonGroup emailCampaign={emailCampaign.length} whatsAppCampaign={whatsAppCampaign.length}/>
         <ClickRatio/>
         <CampaignDetails/>
     </div>

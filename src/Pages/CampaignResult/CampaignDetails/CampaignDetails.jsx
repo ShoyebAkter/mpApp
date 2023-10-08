8b7c5b7d39fbfe7e2382b5a8a264eb3215d57ff3
Shoyeb Akter <heroreal5385@gmail.com>
2023-10-08 11:03:26 +0600

    whatsapp campaign result shown

--- src/Pages/CampaignResult/CampaignDetails/CampaignDetails.jsx
+++ src/Pages/CampaignResult/CampaignDetails/CampaignDetails.jsx
@@ -4,6 +4,7 @@ import { CampaignTypes } from "./CampaignTypes"
 import { Unsubscription } from "./Unsubscription"
 import { useAuthState } from "react-firebase-hooks/auth"
 import { auth } from "../../../firebase.init";
+import { WhatsAppType } from "./WhatsAppType"
 
 export const CampaignDetails = () => {
   const [user]=useAuthState(auth)
@@ -37,13 +38,24 @@ export const CampaignDetails = () => {
   
     return acc;
   }, []);
+  const whatsAppResult = whatsAppCampaign.reduce((acc, campaign) => {
+    const existingCampaign = acc.find((item) => item.campaignType === campaign.campaignType);
+  
+    if (existingCampaign) {
+      existingCampaign.total++;
+    } else {
+      acc.push({ campaignType: campaign.campaignType, total: 1 });
+    }
+  
+    return acc;
+  }, []);
   // console.log(result);
   // const emailCampaignTypeArray=emailCampaign.map((campaign)=>campaign.campaignType)
   return (
     <div className="flex justify-around shadow-2xl rounded-lg m-5">
         <CampaignTypes emailCampaign={emailCampaign.length} whatsAppCampaign={whatsAppCampaign.length} />
         <BounceRate result={result}/>
-        <Unsubscription/>
+        <WhatsAppType whatsAppResult={whatsAppResult}/>
     </div>
   )
 }
---
+++ src/Pages/CampaignResult/CampaignDetails/WhatsAppType.jsx
@@ -0,0 +1,28 @@
+import { Pie } from 'react-chartjs-2';
+
+export const WhatsAppType = ({whatsAppResult}) => {
+    const data = {
+        labels: whatsAppResult.map((data)=>data.campaignType),
+        datasets: [
+          {
+            label: 'No. Of Campaign',
+            data: whatsAppResult.map((data)=>data.total),
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      };
+  return (
+    <div id="chart">
+            <h1 className="text-black text-lg font-bold">Whats App Campaign Type</h1>
+            <Pie data={data} width={350}/>
+        </div>
+  )
+}

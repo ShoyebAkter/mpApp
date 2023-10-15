b7330f3f1bae0e0db87e8fc7b2a633f259fc03dd
Shoyeb Akter <heroreal5385@gmail.com>
2023-10-15 18:36:50 +0600

    getting facebook user likes data

--- src/Pages/Campaign/Preview/Facebook/Facebook.jsx
+++ src/Pages/Campaign/Preview/Facebook/Facebook.jsx
@@ -1,47 +1,24 @@
-
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 export const Facebook = () => {
   const handleFacebookLogin = () => {
-    // Add your Facebook login logic here
     
+
     window.FB.login(function (response) {
+      console.log(response);
       if (response.authResponse) {
-        window.FB.api('/me/accounts', 'GET', { access_token: response.authResponse.accessToken }, function (pagesResponse) {
-          if (pagesResponse && !pagesResponse.error) {
-            // Iterate through the pages and find the Page Access Token and Page ID for the desired page
-            for (let i = 0; i < pagesResponse.data.length; i++) {
-              const page = pagesResponse.data[i];
-              if (page.name === 'YourPageName') { // Replace 'YourPageName' with your page's name or another identifying factor
-                const pageAccessToken = page.access_token;
-                const pageId = page.id;
-    
-                // Now you have the Page Access Token and Page ID for your page
-                console.log('Page Access Token:', pageAccessToken);
-                console.log('Page ID:', pageId);
-    
-                // You can use these tokens to perform actions on your page
-              }
-            }
-          }
-        })
-        // User is logged in, handle the response
-        // console.log(response.authResponse.accessToken);
-        // const userAccessToken=response.authResponse.accessToken;
-        // fetch(`https://graph.facebook.com/me/friends?access_token=${userAccessToken}`)
-        //   .then(response => {console.log(response);response.json()})
-        //   .then(data => {
-        //     // Process the data, find your page, and extract information about it
-        //     const yourPageData = data.data.find(page => page.name === 'Your Page Name');
-        //     console.log('Your Page Data:', data);
-        //   })
-        //   .catch(error => {
-        //     console.error('Error fetching page data:', error);
-        //   });
-      } else {
-        // User cancelled login or did not grant permissions
-        console.log('User cancelled login or did not fully authorize.');
-      }
-    });
+        const userAccessToken = response.authResponse.accessToken;
+        fetch(`https://graph.facebook.com/v18.0/me/likes?access_token=${userAccessToken}`)
+        .then(res=>res.json())
+        .then(res=>console.log(res))
+        // window.FB.api('/me', 'GET', { fields: 'id,name,email,picture', access_token: userAccessToken }, function(userDataResponse) {
+        //   if (!userDataResponse || userDataResponse.error) {
+        //     console.error('Error fetching user data:', userDataResponse.error);
+        //   } else {
+        //     // User data is available in userDataResponse
+        //     console.log('User data:', userDataResponse);
+        //   }
+        // });
+    }})
   };
   // console.log(imageBlob);
   // const shareImageWithText = async () => {
@@ -49,6 +26,7 @@ export const Facebook = () => {
   //     const shareData={
   //         text: text,
   //         files: [new File([imageBlob], 'image.png', { type: imageBlob.type })],
+          
 
   //       }
   //     if (navigator.share) {
@@ -67,12 +45,13 @@ export const Facebook = () => {
 
   return (
     <div>
-      {/* <button 195578516826746 onClick={()=>shareImageWithText()}>Share 🔗</button> */}
+      {/* 195578516826746 */}
+      {/* <button  onClick={()=>shareImageWithText()}>Share 🔗</button> */}
       <button onClick={handleFacebookLogin}>Login with Facebook</button>
     </div>
   )
 }
-// Facebook.propTypes = {
-//   imageBlob: PropTypes.object.isRequired,
-//   text: PropTypes.string.isRequired,
-// }
+Facebook.propTypes = {
+  imageBlob: PropTypes.object.isRequired,
+  text: PropTypes.string.isRequired,
+}

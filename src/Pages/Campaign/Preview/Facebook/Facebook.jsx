f14d4312a1da40a47adcf773764552a7eae9d492
Shoyeb Akter <heroreal5385@gmail.com>
2023-10-16 09:31:41 +0600

    share button added

--- src/Pages/Campaign/Preview/Facebook/Facebook.jsx
+++ src/Pages/Campaign/Preview/Facebook/Facebook.jsx
@@ -1,53 +1,53 @@
 import PropTypes from 'prop-types'
-export const Facebook = () => {
-  const handleFacebookLogin = () => {
+export const Facebook = ({imageBlob,text}) => {
+  // const handleFacebookLogin = () => {
     
 
-    window.FB.login(function (response) {
-      console.log(response);
-      if (response.authResponse) {
-        const userAccessToken = response.authResponse.accessToken;
-        fetch(`https://graph.facebook.com/v18.0/me/likes?access_token=${userAccessToken}`)
-        .then(res=>res.json())
-        .then(res=>console.log(res))
-        // window.FB.api('/me', 'GET', { fields: 'id,name,email,picture', access_token: userAccessToken }, function(userDataResponse) {
-        //   if (!userDataResponse || userDataResponse.error) {
-        //     console.error('Error fetching user data:', userDataResponse.error);
-        //   } else {
-        //     // User data is available in userDataResponse
-        //     console.log('User data:', userDataResponse);
-        //   }
-        // });
-    }})
-  };
+  //   window.FB.login(function (response) {
+  //     console.log(response);
+  //     if (response.authResponse) {
+  //       const userAccessToken = response.authResponse.accessToken;
+  //       fetch(`https://graph.facebook.com/v18.0/me/likes?access_token=${userAccessToken}`)
+  //       .then(res=>res.json())
+  //       .then(res=>console.log(res))
+  //       // window.FB.api('/me', 'GET', { fields: 'id,name,email,picture', access_token: userAccessToken }, function(userDataResponse) {
+  //       //   if (!userDataResponse || userDataResponse.error) {
+  //       //     console.error('Error fetching user data:', userDataResponse.error);
+  //       //   } else {
+  //       //     // User data is available in userDataResponse
+  //       //     console.log('User data:', userDataResponse);
+  //       //   }
+  //       // });
+  //   }})
+  // };
   // console.log(imageBlob);
-  // const shareImageWithText = async () => {
-  //     // const filename = condition ? 'image.jpg' : 'image.png';
-  //     const shareData={
-  //         text: text,
-  //         files: [new File([imageBlob], 'image.png', { type: imageBlob.type })],
-          
+  const shareImageWithText = async () => {
+      // const filename = condition ? 'image.jpg' : 'image.png';
+      
+      if (navigator.share) {
+        try {
+          await navigator.share(
+            {
+          files: [new File([imageBlob], 'image.png', { type: imageBlob.type })],
+          title:"image send"
+        }
+          );
 
-  //       }
-  //     if (navigator.share) {
-  //       try {
-  //         await navigator.share(shareData);
-
-  //       } catch (error) {
-  //         console.error('Error sharing image:', error);
-  //       }
-  //     } else {
-  //       console.log('Web Share API is not supported in this browser.');
-  //     }
-  //   };
+        } catch (error) {
+          console.error('Error sharing image:', error);
+        }
+      } else {
+        console.log('Web Share API is not supported in this browser.');
+      }
+    };
 
 
 
   return (
     <div>
       {/* 195578516826746 */}
-      {/* <button  onClick={()=>shareImageWithText()}>Share 🔗</button> */}
-      <button onClick={handleFacebookLogin}>Login with Facebook</button>
+      <button  onClick={()=>shareImageWithText()}>Share 🔗</button>
+      {/* <button onClick={handleFacebookLogin}>Login with Facebook</button> */}
     </div>
   )
 }

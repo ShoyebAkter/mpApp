123249858ad04815f379c290a827f5ea0e990fe1
Shoyeb Akter <heroreal5385@gmail.com>
2023-12-04 13:50:40 +0600

    header prblm solved

--- src/Pages/LandingPage/Landingpage.jsx
+++ src/Pages/LandingPage/Landingpage.jsx
@@ -10,22 +10,27 @@ import { Introduction } from "./Introduction"
 import { RealTImeUpdates } from "./Realtime/RealTImeUpdates"
 import Header from "../Header/Header"
 import './Introduction.css'
+import { auth } from "../../firebase.init"
+import { useAuthState } from "react-firebase-hooks/auth"
 export const Landingpage = () => {
-  
-  
+  const [user] = useAuthState(auth);
+
   return (
     <div >
       <div >
-      <Header />
-        <section id="introduction"  className='background'><Introduction/></section>
-        <section id="businessInsight"><BusinessInsight/></section>
-       <section id="integration"> <Integration/></section>
-       <section id="realtime"> <Fade damping={0.6}><RealTImeUpdates/></Fade></section>
-       <section id="campaign"> <Campaign/></section>
-        <section id="allies"><Allies/></section>
+        {
+          !user &&
+          <Header />
+        }
+        <section id="introduction" className='background'><Introduction /></section>
+        <section id="businessInsight"><BusinessInsight /></section>
+        <section id="integration"> <Integration /></section>
+        <section id="realtime"> <Fade damping={0.6}><RealTImeUpdates /></Fade></section>
+        <section id="campaign"> <Campaign /></section>
+        <section id="allies"><Allies /></section>
         <section id="contactUs"><Contact /></section>
-        <Footer/>
-    </div>
+        <Footer />
+      </div>
     </div>
   )
 }

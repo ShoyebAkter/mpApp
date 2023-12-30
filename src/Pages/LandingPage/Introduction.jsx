026cc7e145b60e9734877f0f0062881f064eccc3
Shoyeb Akter <heroreal5385@gmail.com>
2023-12-30 11:36:20 +0600

    font family changed

--- src/Pages/LandingPage/Introduction.jsx
+++ src/Pages/LandingPage/Introduction.jsx
@@ -7,8 +7,8 @@ export const Introduction = () => {
                 
                 <div className="flex justify-around items-center">
                     <div className='pt-24'>
-                        <div style={{"color":"#649445"}} className="text-5xl font-medium ">Analytics for business</div>
-                        <div style={{"color":"#62cbc6"}}  className=" text-5xl font-medium py-3">Growth by design</div>
+                        <div style={{"color":"#649445",}} className="firstLine">Analytics for business</div>
+                        <div style={{"color":"#62cbc6"}}  className=" text-5xl font-semibold py-8">Growth by design</div>
                         <div style={{"color":"#6b6b6b"}}  className="text-2xl font-medium">Get more than just numbers.</div>
                         <div style={{"color":"#6b6b6b"}}  className="text-2xl font-medium">Discover insights that</div>
                         <div style={{"color":"#6b6b6b"}}  className="text-2xl font-medium">drive real growth.</div>

2885aa95c117f878d4516d17a1915d4bcb9c05ce
Shoyeb <heroreal5385@gmail.com>
2024-01-18 12:40:30 +0600

    sidebar added

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -4,15 +4,17 @@ import "./Header.css";
 import { Link } from "react-router-dom";
 
 function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  // const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    
-    setMenuOpen(!menuOpen);
+    document.getElementById("menusidebar").style.width = "200px";
+  };
+  const closeSideBar = () => {
+    document.getElementById("menusidebar").style.width = "0px";
   };
   return (
     <header className="header-fixed">
-      <nav className={`navbarSec ${menuOpen ? "open" : ""}`}>
+      <nav className={`navbarSec `}>
         <div className="headerlogo">
           <img src="Logo_Iso_Green.png" />
         </div>
@@ -29,12 +31,41 @@ function Header() {
             LOGIN
           </Link>
         </div>
-        <div className="hamburger-menu" onClick={toggleMenu}>
-          ☰{" "}
+        <div>
+          <a
+            className="hamburger-menu"
+            href="javascript:void(0)"
+            onClick={toggleMenu}
+          >
+            ☰{" "}
+          </a>
+
           {/* You can use an icon or any other content for the hamburger menu */}
         </div>
       </nav>
-      
+      <div className="sidebar" id="menusidebar">
+        <a
+          href="javascript:void(0)"
+          className="close-btn"
+          onClick={closeSideBar}
+        >
+          &times;
+        </a>
+        <div className="links">
+          <a href="#introduction">HOME</a>
+          <a href="#businessInsight">EULERMAIL</a>
+          <a href="#integration">HOW IT WORKS</a>
+          <a href="#allies">OUR ALLIES</a>
+          <a href="#contactUs">CONTACT</a>
+          <Link
+            className="bg-emerald-800 text-white py-2 px-5 mx-8 rounded-xl"
+            to="/login"
+          >
+            LOGIN
+          </Link>
+        </div>
+        
+      </div>
     </header>
   );
 }

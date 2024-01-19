03bdb667298fe1364823cdc1c87f1174470ee555
Shoyeb <heroreal5385@gmail.com>
2024-01-19 19:25:52 +0600

    responsive design changed2

--- src/Pages/Header/LoginHeader.jsx
+++ src/Pages/Header/LoginHeader.jsx
@@ -29,143 +29,156 @@ export const LoginHeader = () => {
   return (
     <div>
       {user ? (
-        <header className={`header-fixed`}>
-          <nav className={`items-center `}>
-            <div style={{ width: "50px" }}>
-              <img src="Logo_Iso_Green.png" />
-            </div>
-            <Link
-              to="/eulermail"
-              onClick={() => handleLinkClick("/eulermail")}
-              className={activeLink === "/eulermail" ? "font-bold" : ""}
-            >
-              EulerMail
-            </Link>
-            <Link
-              to="/businessoverview"
-              onClick={() => handleLinkClick("/businessoverview")}
-              className={activeLink === "/businessoverview" ? "font-bold" : ""}
-            >
-              Business Overview
-            </Link>
-            <Link
-              to="/customerBehaviour"
-              onClick={() => handleLinkClick("/customerBehaviour")}
-              className={activeLink === "/customerBehaviour" ? "font-bold" : ""}
-            >
-              Customer Behaviour
-            </Link>
-            <Link
-              to="/campaignresult"
-              onClick={() => handleLinkClick("/campaignresult")}
-              className={activeLink === "/campaignresult" ? "font-bold" : ""}
-            >
-              Campaign Result
-            </Link>
-            <Link
-              to="/socialmedia"
-              onClick={() => handleLinkClick("/socialmedia")}
-              className={activeLink === "/socialmedia" ? "font-bold" : ""}
-            >
-              Social Media
-            </Link>
-            <Link
-              to="/campaignerdesign"
-              onClick={() => handleLinkClick("/campaignerdesign")}
-              className={
-                activeLink === "/campaignerdesign"
-                  ? "font-bold text-white hover:text-white  py-1 px-4 rounded-full shadow-lg"
-                  : "text-white hover:text-white  py-1 px-4 rounded-full shadow-lg"
-              }
-              style={{
-                background: "#439541",
-                boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.5)",
-              }}
-            >
-              Campaign Designer
-            </Link>
-
-            <button
-              onClick={logout}
-              className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-            >
-              Log Out
-            </button>
-            <div>
-              <a
-                className="hamburger-menu"
-                href="javascript:void(0)"
-                onClick={toggleMenu}
+        <div className="mobileHeader">
+          <header className={`header-fixed`}>
+            <nav className={`navbarSec`}>
+              <div style={{ width: "50px" }}>
+                <img src="Logo_Iso_Green.png" />
+              </div>
+              <Link
+                to="/eulermail"
+                onClick={() => handleLinkClick("/eulermail")}
+                className={activeLink === "/eulermail" ? "font-bold" : ""}
               >
-                ☰{" "}
-              </a>
+                EulerMail
+              </Link>
+              <Link
+                to="/businessoverview"
+                onClick={() => handleLinkClick("/businessoverview")}
+                className={
+                  activeLink === "/businessoverview" ? "font-bold" : ""
+                }
+              >
+                Business Overview
+              </Link>
+              <Link
+                to="/customerBehaviour"
+                onClick={() => handleLinkClick("/customerBehaviour")}
+                className={
+                  activeLink === "/customerBehaviour" ? "font-bold" : ""
+                }
+              >
+                Customer Behaviour
+              </Link>
+              <Link
+                to="/campaignresult"
+                onClick={() => handleLinkClick("/campaignresult")}
+                className={activeLink === "/campaignresult" ? "font-bold" : ""}
+              >
+                Campaign Result
+              </Link>
+              <Link
+                to="/socialmedia"
+                onClick={() => handleLinkClick("/socialmedia")}
+                className={activeLink === "/socialmedia" ? "font-bold" : ""}
+              >
+                Social Media
+              </Link>
+              <Link
+                to="/campaignerdesign"
+                onClick={() => handleLinkClick("/campaignerdesign")}
+                className={
+                  activeLink === "/campaignerdesign"
+                    ? "font-bold text-white hover:text-white  py-1 px-4 rounded-full shadow-lg"
+                    : "text-white hover:text-white  py-1 px-4 rounded-full shadow-lg"
+                }
+                style={{
+                  background: "#439541",
+                  boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.5)",
+                }}
+              >
+                Campaign Designer
+              </Link>
 
-              {/* You can use an icon or any other content for the hamburger menu */}
+              <button
+                onClick={logout}
+                className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+              >
+                Log Out
+              </button>
+              
+            </nav>
+            
+          </header>
+          <div>
+                <a
+                  className="hamburger-menu"
+                  href="javascript:void(0)"
+                  onClick={toggleMenu}
+                >
+                  <img src="/hamburger.png" alt="" />
+                </a>
+
+                {/* You can use an icon or any other content for the hamburger menu */}
+              </div>
+              <div className="sidebar" id="menusidebar">
+              <a
+                href="javascript:void(0)"
+                className="close-btn"
+                onClick={closeSideBar}
+              >
+                &times;
+              </a>
+              <div className="links">
+                <Link
+                  to="/eulermail"
+                  onClick={() => handleLinkClick("/eulermail")}
+                  className={activeLink === "/eulermail" ? "font-bold" : ""}
+                >
+                  EulerMail
+                </Link>
+                <Link
+                  to="/businessoverview"
+                  onClick={() => handleLinkClick("/businessoverview")}
+                  className={
+                    activeLink === "/businessoverview" ? "font-bold" : ""
+                  }
+                >
+                  Business Overview
+                </Link>
+                <Link
+                  to="/customerBehaviour"
+                  onClick={() => handleLinkClick("/customerBehaviour")}
+                  className={
+                    activeLink === "/customerBehaviour" ? "font-bold" : ""
+                  }
+                >
+                  Customer Behaviour
+                </Link>
+                <Link
+                  to="/campaignresult"
+                  onClick={() => handleLinkClick("/campaignresult")}
+                  className={
+                    activeLink === "/campaignresult" ? "font-bold" : ""
+                  }
+                >
+                  Campaign Result
+                </Link>
+                <Link
+                  to="/socialmedia"
+                  onClick={() => handleLinkClick("/socialmedia")}
+                  className={activeLink === "/socialmedia" ? "font-bold" : ""}
+                >
+                  Social Media
+                </Link>
+                <Link
+                  to="/campaignerdesign"
+                  onClick={() => handleLinkClick("/campaignerdesign")}
+                  className={
+                    activeLink === "/campaignerdesign"
+                      ? "font-bold text-white hover:text-white  py-1 px-4 rounded-full shadow-lg"
+                      : "text-white hover:text-white  py-1 px-4 rounded-full shadow-lg"
+                  }
+                  style={{
+                    background: "#439541",
+                    boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.5)",
+                  }}
+                >
+                  Campaign Designer
+                </Link>
+              </div>
             </div>
-          </nav>
-          <div className="sidebar" id="menusidebar">
-        <a
-          href="javascript:void(0)"
-          className="close-btn"
-          onClick={closeSideBar}
-        >
-          &times;
-        </a>
-        <div className="links">
-        <Link
-              to="/eulermail"
-              onClick={() => handleLinkClick("/eulermail")}
-              className={activeLink === "/eulermail" ? "font-bold" : ""}
-            >
-              EulerMail
-            </Link>
-            <Link
-              to="/businessoverview"
-              onClick={() => handleLinkClick("/businessoverview")}
-              className={activeLink === "/businessoverview" ? "font-bold" : ""}
-            >
-              Business Overview
-            </Link>
-            <Link
-              to="/customerBehaviour"
-              onClick={() => handleLinkClick("/customerBehaviour")}
-              className={activeLink === "/customerBehaviour" ? "font-bold" : ""}
-            >
-              Customer Behaviour
-            </Link>
-            <Link
-              to="/campaignresult"
-              onClick={() => handleLinkClick("/campaignresult")}
-              className={activeLink === "/campaignresult" ? "font-bold" : ""}
-            >
-              Campaign Result
-            </Link>
-            <Link
-              to="/socialmedia"
-              onClick={() => handleLinkClick("/socialmedia")}
-              className={activeLink === "/socialmedia" ? "font-bold" : ""}
-            >
-              Social Media
-            </Link>
-            <Link
-              to="/campaignerdesign"
-              onClick={() => handleLinkClick("/campaignerdesign")}
-              className={
-                activeLink === "/campaignerdesign"
-                  ? "font-bold text-white hover:text-white  py-1 px-4 rounded-full shadow-lg"
-                  : "text-white hover:text-white  py-1 px-4 rounded-full shadow-lg"
-              }
-              style={{
-                background: "#439541",
-                boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.5)",
-              }}
-            >
-              Campaign Designer
-            </Link>
         </div>
-        
-      </div>
-        </header>
       ) : null}
     </div>
   );

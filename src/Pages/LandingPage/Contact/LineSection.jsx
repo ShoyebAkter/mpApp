883f762e368849672bf540c9ce5db394610dc07e
Shoyeb <heroreal5385@gmail.com>
2024-01-22 14:22:14 +0600

    contact us pop up added

--- src/Pages/LandingPage/Contact/LineSection.jsx
+++ src/Pages/LandingPage/Contact/LineSection.jsx
@@ -21,8 +21,49 @@ export const LineSection = () => {
         Reality!
       </div>
       <div >
-        <button  className="bg-blue-600 py-2 px-4 rounded border-gray-400 shadow-sm text-gray-200">Contact Us</button>
+        <button onClick={()=>document.getElementById("my_modal_5").showModal()}  className="bg-blue-600 py-2 px-4 rounded border-gray-400 shadow-sm text-gray-200">Contact Us</button>
       </div>
+
+      <dialog id="my_modal_5" className="modal">
+        <div className="modal-box w-1/3  bg-black max-w-full">
+        <div className="p-5 space-y-5 shadow-xl">
+    <h4 className="text-center text-3xl">Contact Us</h4>
+
+    <form>
+      <div className="grid grid-cols-2 gap-5">
+        <input
+          type="text"
+          className="border text-black border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2"
+          readOnly
+          placeholder="To:info@gmail.com"
+        />
+        <input
+          type="email"
+          className="border text-black border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2"
+          placeholder="From"
+        />
+        <textarea
+          cols="10"
+          rows="5"
+          className="border text-black border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2"
+          placeholder="Write your message..."
+        ></textarea>
+      </div>
+      <input
+        type="submit"
+        value="Send Message"
+        className="focus:outline-none mt-5 bg-purple-500 px-4 py-2 text-white font-bold w-full"
+      />
+    </form>
+  </div>
+          {/* <Preview userId={user.uid}  imageBlob={imageBlob} editedImage={editedImage} text={text}/> */}
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };

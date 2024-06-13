c5083b8bcf93d76eb642f56b999e9fc204aad5b3
Shoyeb <heroreal5385@gmail.com>
2024-06-13 17:08:26 +0600

    check 2

--- src/Pages/Authentication/ShopifyAuth.jsx
+++ src/Pages/Authentication/ShopifyAuth.jsx
@@ -3,10 +3,10 @@ import { useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase.init";
 const ShopifyAuth = () => {
-    const [storeUrl,setStoreUrl]=useState("");
-    const [adminApi,setAdminApi]=useState("");
-    const [api,setApi]=useState("")
-  const navigate=useNavigate()
+  const [storeUrl, setStoreUrl] = useState("");
+  const [adminApi, setAdminApi] = useState("");
+  const [api, setApi] = useState("");
+  const navigate = useNavigate();
 
   function generatePassword(length) {
     var charset =
@@ -18,60 +18,56 @@ const ShopifyAuth = () => {
     }
     return password;
   }
-    
-    // Example usage: Generate a password of length 16
-    
-    const onSubmit=async()=>{
-      const companyName=localStorage.getItem("company");
-      const email=localStorage.getItem("shopifyEmail")
-      const subscriptionInfo=localStorage.getItem("subscriptionInfo")
-      const password = generatePassword(16);
-      const subscriptionInfoStr = JSON.parse(subscriptionInfo);
-      // console.log(subscriptionInfoStr)
-// Add the "password" property to the object
-subscriptionInfoStr.password = password; 
-        const shopifyInfo={
-            url:storeUrl,
-            adminApi:adminApi,
-            apiKey:api,
-            companyName:companyName,
-            email:email
-        }
-        await createUserWithEmailAndPassword(auth, email, password)
+
+  // Example usage: Generate a password of length 16
+
+  const onSubmit = async () => {
+    const companyName = localStorage.getItem("company");
+    const email = localStorage.getItem("shopifyEmail");
+    const subscriptionInfo = localStorage.getItem("subscriptionInfo");
+    const password = generatePassword(16);
+    const subscriptionInfoStr = JSON.parse(subscriptionInfo);
+    // console.log(subscriptionInfoStr)
+    // Add the "password" property to the object
+    subscriptionInfoStr.password = password;
+    const shopifyInfo = {
+      url: storeUrl,
+      adminApi: adminApi,
+      apiKey: api,
+      companyName: companyName,
+      email: email,
+    };
+    await createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
         // console.log(password);
         return auth.signOut().then(() => {
-          
           fetch("https://emapp-backend.vercel.app/subscriptionemail", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
             },
             body: JSON.stringify(subscriptionInfoStr),
-          })
+          }).then((res) => {
+            if (res.status === 200) {
+              // console.log(res)
+              fetch("https://emapp-backend.vercel.app/sendsubscriptionemail", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(subscriptionInfoStr),
+              }).then((res) => {
+                if(res.status===200){
+                  navigate('/login')
+                }
+              });
+            }
+          });
           // Additional actions after sign out if needed
-
-          fetch("https://emapp-backend.vercel.app/sendsubscriptionemail", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(subscriptionInfoStr),
-          })
-
-          fetch("https://emapp-backend.vercel.app/subscription/database", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(subscriptionInfoStr),
-          })
-
-
         });
-        
+
         // ...
       })
       .catch((error) => {
@@ -82,23 +78,36 @@ subscriptionInfoStr.password = password;
       });
       
         fetch("https://emapp-backend.vercel.app/shopify/info", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(shopifyInfo),
-    }).then((res) => {
-      if (res.status === 200) {
-        navigate('/login')
-      }
-    });
-    }
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(shopifyInfo),
+            })
+        //     .then((res) => {
+        //   if (res.status === 200) {
+        //     // fetch(
+        //     //   "https://emapp-backend.vercel.app/subscription/database",
+        //     //   {
+        //     //     method: "POST",
+        //     //     headers: {
+        //     //       "Content-Type": "application/json",
+        //     //     },
+        //     //     body: JSON.stringify(subscriptionInfoStr),
+        //     //   }
+        //     // )
+        //   }
+        // });
+      
+  };
   return (
     <div className="loginsection">
       <section>
         <img className="mx-auto h-72" src="/logo.png" />
         <div className=" bg-slate-50 shadow-xl px-12 py-10 rounded-3xl mb-5">
-          <h1 className="text-2xl font-bold text-center py-2">Shopify Authorization</h1>
+          <h1 className="text-2xl font-bold text-center py-2">
+            Shopify Authorization
+          </h1>
           <div>
             <div className="text-xl font-medium py-2">
               To allow us to securely connect to your Shopify data, you must
@@ -123,7 +132,6 @@ subscriptionInfoStr.password = password;
               </div>
               <div>
                 <label
-                
                   htmlFor=""
                   className="text-xl  py-2 block tracking-wide leading-6 font-semibold text-gray-400"
                 >
@@ -131,7 +139,7 @@ subscriptionInfoStr.password = password;
                   <span className="text-red-400">*</span>
                 </label>
                 <input
-                onChange={(e)=>setStoreUrl(e.target.value)}
+                  onChange={(e) => setStoreUrl(e.target.value)}
                   type="text"
                   placeholder="Url"
                   required
@@ -148,7 +156,7 @@ subscriptionInfoStr.password = password;
                 </label>
                 <input
                   type="text"
-                  onChange={(e)=>setAdminApi(e.target.value)}
+                  onChange={(e) => setAdminApi(e.target.value)}
                   placeholder=""
                   required
                   className="appearance-none bg-white w-full py-2 rounded-md border-gray-300"
@@ -164,8 +172,7 @@ subscriptionInfoStr.password = password;
                 </label>
                 <input
                   type="text"
-                  
-                  onChange={(e)=>setApi(e.target.value)}
+                  onChange={(e) => setApi(e.target.value)}
                   placeholder=""
                   required
                   className="appearance-none bg-white w-full py-2 rounded-md border-gray-300"
@@ -173,7 +180,12 @@ subscriptionInfoStr.password = password;
               </div>
             </div>
           </div>
-          <button className="shadow-xl text-white bg-sky-600 font-medium rounded-full text-sm px-7 py-2 text-center mr-2 mb-2" onClick={onSubmit}>Submit</button>
+          <button
+            className="shadow-xl text-white bg-sky-600 font-medium rounded-full text-sm px-7 py-2 text-center mr-2 mb-2"
+            onClick={onSubmit}
+          >
+            Submit
+          </button>
         </div>
       </section>
     </div>

7604c5efd1f2f7011d4530c2e6ca101ea3474d9c
Shoyeb <heroreal5385@gmail.com>
2025-02-08 19:29:14 +0600

    campaign new img added

--- src/Pages/LandingPage/CampaignCreation/CampaignLeftSection.jsx
+++ src/Pages/LandingPage/CampaignCreation/CampaignLeftSection.jsx
@@ -2,7 +2,7 @@ import './Campaign.css'
 export const CampaignLeftSection = () => {
   return (
     <div>
-        <img className="rounded-xl w-[600px] campaignImg" src="/campaign.png" alt=""/>
+        <img className="rounded-xl w-[600px] campaignImg" src="/campaginNew.png" alt=""/>
     </div>
   )
 }
